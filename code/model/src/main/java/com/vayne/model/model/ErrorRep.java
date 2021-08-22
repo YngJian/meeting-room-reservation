b5804170bddf2cc4211ba589224d4f19718090ef
Yang Jian <yangjian1011>
2021-08-22 17:46:35 +0800

    fix: 修改全局错误返回结构
    [Ticket] N/A
    [Description] 修改全局错误返回结构

---
+++ code/model/src/main/java/com/vayne/model/model/ErrorRep.java
@@ -0,0 +1,19 @@
+package com.vayne.model.model;
+
+import com.vayne.model.common.Result;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * @author : Yang Jian
+ * @date : 2021/7/6 0006 22:06
+ */
+@Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorRep {
+    private Result result;
+}
